dbca84511963a5d306a6d6cb0cd524ed3c7eb4d0
RaphaelMetz <raphaeljmetz@gmail.com>
2017-12-07 19:20:10 -0500

    Using the old build file (bad) Started drivesystem

---
+++ src/org/usfirst/frc/team1389/systems/OctoMecanumSystem.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc.team1389.systems;
+
+import com.team1389.system.drive.DriveSystem;
+import com.team1389.util.list.AddList;
+import com.team1389.watch.Watchable;
+
+public class OctoMecanumSystem extends DriveSystem
+{
+	
+	
+
+	@Override
+	public AddList<Watchable> getSubWatchables(AddList<Watchable> stem)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getName()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void init()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
